2c1faf0836de1ad51ad4b0a0047c3ee5b6192d02
Hexar07 <hexar06@gmail.com>
2026-03-01 22:58:43 +0100

    gotowe zmiany le jeszcze do poprawy

--- Assets/Scripts/ui/MainMenu.cs
+++ Assets/Scripts/ui/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
@@ -13,6 +14,7 @@ public class MainMenu : MonoBehaviour
     {
         CreateNewGame.DeleteSeve();
         CreateNewGame.CreateSeve();
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void LoadGame()
